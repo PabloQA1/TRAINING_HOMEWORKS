68f26a085830571f306d8f5fbdfa7eaf964fdc33
Pavel Novukau1 <pavel.novikau.qa@gmail.com>
2022-05-04 22:36:15 +0300

    HW AT #2 added to the study project.

---
+++ src/test/java/AT/hw_2/BasePageTest.java
@@ -0,0 +1,33 @@
+package AT.hw_2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+public abstract class BasePageTest {
+
+   protected WebDriver driver;
+   protected LoginPage loginPage;
+   protected MailPage mailPage;
+
+    @BeforeMethod
+    public void setUp() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+            loginPage = new LoginPage(driver);
+            mailPage = new MailPage(driver);
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+}
